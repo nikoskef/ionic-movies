27cf91a056cad86640c1218b266d13c7d5b4b453
Nikos Kefalas <nikoskef@outlook.com>
2022-03-16 15:28:24 +0200

    build prod and keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  key: 'e7d5e32972aa8ef7850d412915ffe85e',
+  api: 'https://api.themoviedb.org/3',
+  images: 'https://image.tmdb.org/t/p'
 };
